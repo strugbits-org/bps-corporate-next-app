9674c1e76adeb813de4c0ec6b05ba244f77e0bd4
osyed002 <osyed@strugbitsglobal.com>
2024-04-29 18:35:30 +0500

    speed insights and 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,37 @@
-import { markPageLoaded } from "@/utils/utilityFunctions"
-
+import { markPageLoaded } from "@/utils/utilityFunctions";
 export default function Custom404() {
-    markPageLoaded();
-    return (
-        <>
-            <section class="section-error-404">
-                <div class="container-title">
-                    <h1 class="fs--900 blue-1 split-chars words chars splitting aos-animate" data-aos="d:loop">404</h1>
-                    <span class="fs--20 fw-600 text-uppercase aos-animate fadeIn" data-aos="fadeIn .8s ease-in-out .6s">Page not found</span>
-                </div>
-            </section></>
-    )
+  markPageLoaded();
+  return (
+    <>
+      <section className="section-error-404">
+        <div className="container-title">
+          <h1
+            className="fs--900 blue-1 split-chars words chars splitting aos-animate"
+            data-aos="d:loop"
+          >
+            404
+          </h1>
+          <span
+            className="fs--20 fw-600 text-uppercase aos-animate fadeIn"
+            data-aos="fadeIn .8s ease-in-out .6s"
+          >
+            Page not found
+          </span>
+        </div>
+      </section>
+      <style jsx>{`
+        .fs--900 {
+          font-size: 20rem;
+        }
+        .fs--20 {
+          font-size: 1.5rem;
+        }
+        @media screen and (min-width: 1024px) {
+          .fs--900 {
+            font-size: 84rem;
+          }
+        }
+      `}</style>
+    </>
+  );
 }
