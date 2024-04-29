ed56cb7f91e8e8406a330f0ae61d05bddd69eaef
osyed002 <osyed@strugbitsglobal.com>
2024-04-29 18:15:49 +0500

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,5 +2,13 @@ import { markPageLoaded } from "@/utils/utilityFunctions"
 
 export default function Custom404() {
     markPageLoaded();
-    return <h1>404 - Page Not Found</h1>
-  }
+    return (
+        <>
+            <section class="section-error-404">
+                <div class="container-title">
+                    <h1 class="fs--900 blue-1 split-chars words chars splitting aos-animate" data-aos="d:loop">404</h1>
+                    <span class="fs--20 fw-600 text-uppercase aos-animate fadeIn" data-aos="fadeIn .8s ease-in-out .6s">Page not found</span>
+                </div>
+            </section></>
+    )
+}
